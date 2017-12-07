d3553d00e66b69d03f149373a3ba568478b23d7b
Mark Wood <markozywood@gmail.com>
2017-12-07 20:06:57 +0000

    Remove old vim dotfiles

--- .vim/install.sh
+++
@@ -1,9 +0,0 @@
-mkdir bundle
-
-cd bundle
-
-git clone https://github.com/tpope/vim-sensible.git
-git clone https://github.com/tpope/vim-vinegar.git
-git clone https://github.com/altercation/vim-colors-solarized.git
-
-cd ..
