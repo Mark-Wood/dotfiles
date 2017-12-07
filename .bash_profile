746509243ddf654ec1b73e74d45b7131e93336ee
Mark Wood <markozywood@gmail.com>
2017-12-07 15:09:05 +0000

    Add bashrc and fzf dotfiles

--- .bash_profile
+++ .bash_profile
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export PATH=$HOME/bin:$PATH
-
-export PS1='\[\033[01;32m\]\u\[\033[00m\]@\[\033[01;32m\]\h\[\033[00m\]:\[\033[00m\]\[\033[01;34m\]\w\[\033[00m\]$\[\033[00m\] '
-
 if [ -e ~/.bashlocal ]; then
     . ~/.bashlocal
 fi
---
+++ .fzf.bash
@@ -0,0 +1,18 @@
+# Setup fzf
+# ---------
+
+if [ -e ~/.linuxbrew/opt/fzf/bin ]; then
+  _append_to_path ~/.linuxbrew/opt/fzf/bin
+  source ~/.linuxbrew/opt/fzf/shell/completion.bash 2> /dev/null
+  source ~/.linuxbrew/opt/fzf/shell/key-bindings.bash
+fi
+
+if _has fzf && _has ag; then
+  export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_DEFAULT_OPTS='
+  --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
+  --color info:108,prompt:109,spinner:108,pointer:168,marker:168
+  '
+fi
