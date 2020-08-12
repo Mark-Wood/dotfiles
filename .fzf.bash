6a7b8917ead5353b35e3d7c514dbf488f1c7ad3b
Mark Wood <mark.wood@ovoenergy.com>
2020-08-12 02:20:56 +0100

    Update path to FZF default config files

--- .fzf.bash
+++ .fzf.bash
@@ -1,17 +1,13 @@
 # Setup fzf
 # ---------
 
-if [ -e /home/linuxbrew/.linuxbrew/opt/fzf/bin ]; then
-  source /home/linuxbrew/.linuxbrew/opt/fzf/shell/completion.bash 2> /dev/null
-  source /home/linuxbrew/.linuxbrew/opt/fzf/shell/key-bindings.bash
+if [ -e /usr/bin/fzf ]; then
+  source /usr/share/doc/fzf/examples/completion.bash 2> /dev/null
+  source /usr/share/doc/fzf/examples/key-bindings.bash
 fi
 
 if _has fzf && _has ag; then
   export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
   export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
-  export FZF_DEFAULT_OPTS='
-  --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
-  --color info:108,prompt:109,spinner:108,pointer:168,marker:168
-  '
 fi
